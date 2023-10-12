eeb6dac695c5c48d6038c63bd0a5a6c67e55c8db
6godpro <erickson5115@gmail.com>
2023-10-12 21:04:16 +0100

    added not null constraint

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,7 +2,7 @@
 -- id, email, name, country
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) UNIQUE,
+    email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') DEFAULT 'US'
 );
