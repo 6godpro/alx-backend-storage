021042c97fffc7c1f9ac5eb08b8ce26f86b20678
6godpro <erickson5115@gmail.com>
2023-10-12 23:36:29 +0100

    fix: cleaned up

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,7 @@
 --  create a trigger that decreases the quantity of an item after adding a new order.
-DELIMITER //
 CREATE TRIGGER order_item_created
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE holberton.items SET quantity=(quantity - NEW.number) WHERE name=NEW.item_name;
 END;
-//
-DELIMITER ;
