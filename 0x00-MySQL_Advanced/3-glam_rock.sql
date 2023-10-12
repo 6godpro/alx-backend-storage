0fddc0ee112adf91e365b67bb60d7d01471562b6
6godpro <erickson5115@gmail.com>
2023-10-12 22:09:12 +0100

    initial commit

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,12 @@
+-- Lists all bands with Glam rock as their main style,
+-- ranked by their longevity
+SELECT band_name,
+CASE
+    WHEN split IS NULL THEN 2022
+    ELSE split
+END
+-- An alternative to CASE-END syntax is to use COALESCE function
+- formed as lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
