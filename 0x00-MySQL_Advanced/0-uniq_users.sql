c1ea43613ad8d986a15bdb7aa224b0287da70370
6godpro <erickson5115@gmail.com>
2023-10-12 20:44:47 +0100

    removed creation of database from script

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,5 @@
--- create database holberton on the server
-CREATE DATABASE IF NOT EXISTS holberton;
--- switch to the holberton db
-USE holberton;
--- create a table called users
+-- create a table called users with the fields
+-- id, email, name
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
