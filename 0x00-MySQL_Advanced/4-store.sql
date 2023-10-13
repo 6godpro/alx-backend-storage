8c87098ba6e17079039c83e61e314ed697489907
6godpro <erickson5115@gmail.com>
2023-10-13 10:42:18 +0100

    fix: removed BEGIN and END keyword

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,7 +1,6 @@
+-- Active: 1694002041516@@127.0.0.1@3306@holberton
 --  create a trigger that decreases the quantity of an item after adding a new order.
 CREATE TRIGGER order_item_created
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
-    UPDATE holberton.items SET quantity=(quantity - NEW.number) WHERE name=NEW.item_name;
-END;
+UPDATE items SET quantity = (quantity - NEW.number) WHERE name=NEW.item_name;
